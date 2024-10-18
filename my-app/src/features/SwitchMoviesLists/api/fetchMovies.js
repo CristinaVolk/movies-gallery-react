6c015bbf21f590cf2566c98e59155940e6e513b7
CristinaVolk <kristinavolk4@gmail.com>
2024-10-18 23:13:30 +0100

    project reset

--- my-app/src/features/SwitchMoviesLists/api/fetchMovies.js
+++
@@ -1,9 +0,0 @@
-import {movies} from "./data/movies";
-
-export function fetchMovies() {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=> {
-            resolve({ok: true, data: movies})
-        }, 1000)
-    })
-}
